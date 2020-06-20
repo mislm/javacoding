7720ae2e79b201a55f244564b1dac5abe760ef45
Shamim <Mohdmonirul1976@gmail.com>
2020-06-20 03:16:57 -0400

    project4

---
+++ src/truthtable/EvenOdd.java
@@ -0,0 +1,33 @@
+package truthtable;
+
+public class EvenOdd {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 20;
+		
+		
+		if(num1 % 2 == 0) {
+			System.out.println(num1 + " is the even");
+			
+		}else
+			System.out.println(num1 + " is the odd ");
+		
+		
+	
+	
+	
+	
+	 int num2 = 45;
+	 
+	 if(num2 % 2 == 0) {
+		 
+		 System.out.println(num2 + " is the even");
+		 
+	 }else
+		 System.out.println(num2 + " is the odd");
+	 
+}
+
+
+}
---
+++ src/truthtable/FindNumber.java
@@ -0,0 +1,127 @@
+package truthtable;
+
+public class FindNumber {
+	
+	public static void main(String[]args) {
+		
+		int num1 = 100;
+		
+		int num2  = 200;
+		
+		if(num1 < num2) {
+			
+			System.out.println("num1 "+ num1 + " is less than " + "num2 "+ num2);
+			
+		}else {
+			
+			System.out.println("num2 "+ num2 + " is gearter than "+ "num1 "+ num1);
+		}	
+	
+
+
+       int num3 = 100;
+       
+       int num4 = 60;
+       
+       if(num3 > num4) {
+    	   
+    	   System.out.println("num3 " + num3 + " is the greater than "+ "num4 "+ num4);
+    	   
+       }else {
+    	   System.out.println("num4 "+ num4 + " is greater than" + "num4 "+ num4);
+       }
+
+	
+	
+	
+
+       int x = 20;
+       
+       int y = 10;
+       
+       if(x > y) {
+    	   System.out.println("x " + x + " is the greater than "+ " y "+ y);
+    	   
+       }else
+    	   System.out.println("y " + y + " is the greater than "+ " x "+ x);
+       
+       
+         
+       int a = 30;
+       int b = 50;
+       
+       if(b > a) {
+    	   
+    	   System.out.println("b " + b + " is the greater than "+ " a "+ a);
+    		   
+       }else
+    	   System.out.println("a " + a + " is the greater than "+ " b "+ b);
+       
+	
+	
+
+
+
+       int s = 20;
+       int h = 10;
+       int f = 30;
+       
+       if(s >= h && s >= f) {
+    	   System.out.println("s "+ s + " is the greatest");
+    	   
+       }else if(h >= s && h >= f) {
+    	   System.out.println("h "+ h + " is the greatest");
+    
+       }else
+    	   System.out.println("f "+ f + " is the greatest");
+       
+       
+       
+       
+       
+       
+       }
+	
+	
+	
+	
+	
+	
+	
+       }
+       
+    	   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
